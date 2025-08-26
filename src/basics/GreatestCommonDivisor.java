5b83ad08ee801548bf56c68a272e486352e4abdd
shubhammalve1 <shubhammalve098@gmail.com>
2025-08-26 13:35:12 +0530

    PrimeNumber Code is modified!

--- src/basics/GreatestCommonDivisor.java
+++ src/basics/GreatestCommonDivisor.java
@@ -7,7 +7,7 @@ import java.util.Set;
 
 public class GreatestCommonDivisor {
     public static void main(String[] args){
-        GCD(4,6);
+        GCDWay2(11,13);
     }
 
     public static int GCD(int n1, int n2){
@@ -32,5 +32,16 @@ public class GreatestCommonDivisor {
         divisorsNumber1.retainAll(divisorsNumber2);
         System.out.println(divisorsNumber1);
         return 0;
+    }
+
+    public static void GCDWay2(int n1, int n2){
+        int gcd = 1;
+        for(int i = 1; i*i<=Math.max(n1,n2); i++){
+            if(n1 % i == 0 && n2 % i == 0){
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD: " + gcd);
     }
 }
--- src/basics/PrimeNumber.java
+++ src/basics/PrimeNumber.java
@@ -2,7 +2,8 @@ package basics;
 
 public class PrimeNumber {
     public static void main(String[] args){
-        System.out.println(isPrime(7));
+        //System.out.println(isPrime(7));
+        System.out.println(isPrimeOptimized(5));
     }
 
     public static boolean isPrime(int number){
@@ -15,6 +16,25 @@ public class PrimeNumber {
         }
 
        return false;
+    }
+
+    public static boolean isPrimeOptimized(int number){
+        int count = 0;
+        for(int i = 1; i*i<number; i++) {
+            if(number % i == 0){
+                count++;
+
+                if(number/i != i){
+                    count++;
+                }
+            }
+        }
+
+        if(count == 2){
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }

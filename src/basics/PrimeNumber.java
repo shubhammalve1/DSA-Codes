6ff4fa272e1abfbdf23fc58d7ce3087e396e74c2
shubhammalve1 <shubhammalve098@gmail.com>
2025-08-26 13:01:39 +0530

    PrimeNumber code is Done!

--- src/basics/PrimeNumber.java
+++ src/basics/PrimeNumber.java
@@ -1,5 +1,20 @@
 package basics;
 
 public class PrimeNumber {
+    public static void main(String[] args){
+        System.out.println(isPrime(7));
+    }
+
+    public static boolean isPrime(int number){
+        for(int i = 2; i<= number; i++){
+            if(number % i == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+       return false;
+    }
 
 }
